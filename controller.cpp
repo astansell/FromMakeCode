b8b3e47146c667b65f4c8a836cc9ca1006a75c44
astansell <33355480+astansell@users.noreply.github.com>
2023-07-07 14:26:04 -0400

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -1,1 +1,27 @@
+#include "pxt.h"
+using namespace pxt;
 
+/**
+ * Blocks for Controller
+ */
+//% color=#0000FF weight=100
+namespace PLTW_Controller {
+
+    bool initialized = false;
+
+    //%
+    void init() {
+        if (initialized) return;
+        //This function sets all the buttons on the Controller to actually appear as buttons on the micro:bit
+        #define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
+            ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
+        #undef ALLOC_PIN_BUTTON
+
+        initialized = true;
+    }
+}
